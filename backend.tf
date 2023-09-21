4801ad106a556387efb23cde7054e038c7c9a19b
caglarkurtkaya <kurtkayacaglar@gmail.com>
2023-09-21 13:30:27 -0500

    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -5,12 +5,13 @@ terraform {
       version = "=2.91.0"
     }
   }
+}
 
-
-backend "azurerm" {
+/*backend "azurerm" {
         resource_group_name  = "wordpressResourceGroup" #resource resource_group_name created manually 
         storage_account_name = "wordpressbackend822"     # created manually
         container_name       = "backend"
         key                  = "wordpress.tfstate"
     }
 }
+*/
